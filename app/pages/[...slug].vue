1e0cc3c84b1397da46d5b0952ea48dbe9dd3c7db
Ellipse120 <ellipse120@gmail.com>
2025-04-08 22:16:09 +0800

    feat: add catch all slug page

---
+++ app/pages/[...slug].vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <UCard>
+      <template #header>
+        <span class="text-3xl font-bold">Not Found</span>
+      </template>
+
+      <UAlert
+        color="warning"
+        title="Heads up!"
+        :description="`You are visiting ${$route.params.slug}, seems doesn't exist!`"
+        icon="i-lucide-terminal"
+      />
+
+      <template #footer>
+        <NuxtLink to="/">
+          <UButton color="primary" icon="i-lucide-arrow-left" variant="subtle">
+            Back To Home Page
+          </UButton>
+        </NuxtLink>
+      </template>
+    </UCard>
+  </div>
+</template>
