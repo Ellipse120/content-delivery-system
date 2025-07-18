3158a8b1de397826897dd467ec9fd74a2c70efd4
Ellipse120 <ellipse120@gmail.com>
2025-07-18 18:01:21 +0800

    build: add pm2 example config

---
+++ ecosystem.config.example.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'CDS',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: '1',
+      script: './.output/server/index.mjs',
+      env: {
+        NUXT_HOSPITAL_TOKEN: '',
+      },
+    },
+  ],
+}
