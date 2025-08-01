2701fa324c88d88c5ad92d5f94f18b7d41d6ae55
Ellipse120 <ellipse120@gmail.com>
2025-08-01 16:35:17 +0800

    chore: update nuxt and @nuxt/ui

--- app/app.vue
+++ app/app.vue
@@ -2,11 +2,24 @@
   <NuxtLoadingIndicator />
   <UApp>
     <NuxtRouteAnnouncer />
-    <NuxtPage />
+
+    <UContainer>
+      <UNavigationMenu
+        highlight
+        highlight-color="success"
+        orientation="horizontal"
+        :items="items"
+        trailing-icon="i-lucide-arrow-down"
+        class="data-[orientation=horizontal]:border-b border-(--ui-border) data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-48"
+      />
+      <NuxtPage />
+    </UContainer>
   </UApp>
 </template>
 
 <script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
 useSeoMeta({
   title: 'Content Delivery System',
   description:
@@ -14,4 +27,46 @@ useSeoMeta({
   ogSiteName: 'Content Delivery System',
   ogType: 'website',
 })
+
+const items = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Sites Map',
+      icon: 'i-lucide-book-open',
+      active: true,
+      children: [
+        {
+          label: 'Clients Page',
+          description: 'Manage All Clients Information',
+          icon: 'i-lucide-house',
+          to: '/clients',
+        },
+        {
+          label: 'Assets Page',
+          description: 'Manage All Assets, including ads image and video link',
+          icon: 'i-lucide-cloud-download',
+          to: '/assets',
+        },
+      ],
+    },
+    {
+      label: 'Misc',
+      icon: 'i-lucide-microscope',
+      children: [
+        {
+          label: '2073 Page',
+          description: 'Fetch doctor scheduling infomation of some special day',
+          icon: 'i-lucide-hospital',
+          to: '/2073',
+        },
+        {
+          label: 'Local Ollama',
+          description: 'Interactive with local ollama, call it\'s embeded API points',
+          icon: 'i-lucide-flask-round',
+          to: '/demo/ollama-chat',
+        },
+      ],
+    },
+  ],
+])
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,59 +1,8 @@
 <script lang="ts" setup>
-import type { NavigationMenuItem } from '@nuxt/ui'
-
-const items = ref<NavigationMenuItem[][]>([
-  [
-    {
-      label: 'Sites Map',
-      icon: 'i-lucide-book-open',
-      active: true,
-      children: [
-        {
-          label: 'Clients Page',
-          description: 'Manage All Clients Information',
-          icon: 'i-lucide-house',
-          to: '/clients',
-        },
-        {
-          label: 'Assets Page',
-          description: 'Manage All Assets, including ads image and video link',
-          icon: 'i-lucide-cloud-download',
-          to: '/assets',
-        },
-      ],
-    },
-    {
-      label: 'Misc',
-      icon: 'i-lucide-microscope',
-      children: [
-        {
-          label: '2073 Page',
-          description: 'Fetch doctor scheduling infomation of some special day',
-          icon: 'i-lucide-hospital',
-          to: '/2073',
-        },
-        {
-          label: 'Local Ollama',
-          description: 'Interactive with local ollama, call it\'s embeded API points',
-          icon: 'i-lucide-flask-round',
-          to: '/demo/ollama-chat',
-        },
-      ],
-    },
-  ],
-])
 </script>
 
 <template>
-  <UContainer>
-    <UNavigationMenu
-      highlight
-      highlight-color="success"
-      orientation="horizontal"
-      :items="items"
-      trailing-icon="i-lucide-arrow-down"
-      class="data-[orientation=horizontal]:border-b border-(--ui-border) data-[orientation=horizontal]:w-full data-[orientation=vertical]:w-48"
-    />
+  <div>
     <USeparator
       color="primary"
       type="dashed"
@@ -66,7 +15,7 @@ const items = ref<NavigationMenuItem[][]>([
     <UButton @click="navigateTo('/demo/ollama-chat')">
       To Local Ollama Chat Demo Page
     </UButton>
-  </UContainer>
+  </div>
 </template>
 
 <style></style>
